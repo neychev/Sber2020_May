558fa0d3a1e9645ac51e67242a188c1595a23740
neychev <neichevxxx@gmail.com>
2020-05-22 00:47:32 +0300

    added lecture materials

--- day02/notmnist.py
+++ day02/notmnist.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from scipy.misc import imread,imresize
+from matplotlib.pyplot import imread
 from sklearn.model_selection import train_test_split
 from glob import glob
 
@@ -21,7 +21,7 @@ def load_notmnist(path='./notMNIST_small',letters='ABCDEFGHIJ',
         class_i = img_path.split(os.sep)[-2]
         if class_i not in letters: continue
         try:
-            data.append(imresize(imread(img_path), img_shape))
+            data.append(imread(img_path))
             labels.append(class_i,)
         except:
             print("found broken img: %s [it's ok if <10 images are broken]" % img_path)
